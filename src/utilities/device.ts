3278e181b0da49dd6b2decffc834713d1db9ae50
Abhishek Dallas <abhishekdallasalpy@gmail.com>
2024-07-07 00:27:23 +0530

    Add utility functions related to device

---
+++ src/utilities/device.ts
@@ -0,0 +1,47 @@
+import req from "../api/requests";
+import { DEVICE_INFO_URL } from "../constants/api";
+
+export function getDeviceIdFromJwt(token: string) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join(""),
+  );
+
+  return JSON.parse(jsonPayload).deviceId[0].id;
+}
+
+export async function getDeviceIp(id: number): Promise<string> {
+  const deviceIpFromLocalStorage =
+    localStorage.getItem("deviceIp") ?? "0.0.0.0";
+  const requestCheck: Promise<string> = new Promise((resolve, reject) => {
+    req
+      .get(`http://${deviceIpFromLocalStorage}/res`)
+      .then((res) => {
+        if (res.success) resolve(deviceIpFromLocalStorage);
+      })
+      .catch((err) => reject(err));
+  });
+  const serverResponse: Promise<string> = new Promise((resolve, reject) => {
+    req
+      .get(`${DEVICE_INFO_URL}?id=${id}`)
+      .then((res) => {
+        if (res.success) resolve(res.data.deviceInfo.ip);
+      })
+      .catch((err) => reject(err));
+  });
+  
+  try {
+    const ip = await Promise.any([requestCheck, serverResponse]);
+    localStorage.setItem("deviceIp", ip);
+    return ip;
+  } catch (err) {
+    throw new Error((err as string).toString());
+  }
+}
